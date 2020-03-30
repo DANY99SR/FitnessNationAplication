7708b5d82d15821f1ce85285da7a3213d510d14b
Szilvia <szilviakovacs55@gmail.com>
2020-03-30 19:45:29 +0300

    commit pamapama

--- app/src/main/java/com/example/fitnessnationaplication/MainActivity.kt
+++ app/src/main/java/com/example/fitnessnationaplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
-        //lalalalalalal
+        //pmapmapama
     }
 }
