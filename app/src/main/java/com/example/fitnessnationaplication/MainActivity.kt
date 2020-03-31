8a1b35a7811424468e26a65a26c0750a7ff52a96
Szilvia <szilviakovacs55@gmail.com>
2020-03-31 08:40:25 +0300

    hey

--- app/src/main/java/com/example/fitnessnationaplication/MainActivity.kt
+++ app/src/main/java/com/example/fitnessnationaplication/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
+        //heeeey
 
     }
 }
