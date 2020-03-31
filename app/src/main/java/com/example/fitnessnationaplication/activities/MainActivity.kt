1ade49c3f074c0fe1bad101ac3ef42c42dcdc05c
Cozma Daniel <cozma.daniel99@yahoo.com>
2020-03-31 10:42:03 +0300

    Am facut cele 2 activitati

---
+++ app/src/main/java/com/example/fitnessnationaplication/activities/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.fitnessnationaplication.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitnessnationaplication.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_go_to_sessions.setOnClickListener {
+            gotoSessionActivity()
+        }
+
+
+    }
+
+    private fun gotoSessionActivity(){
+        val intent = Intent(this, FinessNationSessionActivity::class.java)
+        startActivity(intent)
+    }
+}
